afd605bcfc8d3b26c8ad67abb36ddb3161fa28e2
Combifightet <47188809+Combifightet@users.noreply.github.com>
2025-11-10 18:02:58 +0100

    chore: change dfeault house size

--- scripts/main.gd
+++ scripts/main.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	#floorplan_gen.set_seed(7)
 	randomize()
 	floorplan_gen.set_seed(randi())
-	floorplan_gen.generate(FloorPlanGen.HouseSize.SMALL)
+	floorplan_gen.generate(FloorPlanGen.HouseSize.NORMAL)
 	print("last_seed: ", floorplan_gen.get_last_seed())
 
 	print("displaying grid ...")
