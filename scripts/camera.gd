5136fa635e2909f2a43910a9e5ac30b60740048c
Combifightet <malte@hei-sharp-s.de>
2025-11-14 14:36:51 +0100

    fix: increase shadow quality

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,18 +1,19 @@
 extends Node3D
 
-@export var camera_deadzone: float = 1.0  # This variable is unused in your active code
-@export var camera_follow_speed: float = 5.0 # Higher = smoother camera
-@export var camera_rotation_duration: float = 0.3  # Duration of 90-degree rotation
+@export var camera_deadzone: float = 1.0
+@export var camera_follow_speed: float = 5.0
+@export var camera_rotation_duration: float = 0.3
 
-#@onready var camera: Camera3D = $Camera3D 
 @onready var player: Node3D = %Player
 
-var _target_camera_rotation: float = 0.0
+var _current_angle: float = 0.0
+var _target_angle: float = 0.0
 var _is_rotating_camera: bool = false
 
 
 func _process(delta: float) -> void:
 	global_position = lerp(global_position, player.global_position, camera_follow_speed * delta)
+	rotation.y = _current_angle
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("rotate_camera_left"):
@@ -26,13 +27,17 @@ func rotate_camera(degrees: float) -> void:
 		return
 		
 	_is_rotating_camera = true
-	_target_camera_rotation += deg_to_rad(degrees)
+	_target_angle = _current_angle + deg_to_rad(degrees)
 	
 	var tween = create_tween()
-
-	tween.tween_property(self, "rotation:y", _target_camera_rotation, camera_rotation_duration)
-
+	tween.tween_property(self, "_current_angle", _target_angle, camera_rotation_duration)
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_IN_OUT)
 	
-	tween.finished.connect(func(): _is_rotating_camera = false)
+	tween.finished.connect(_finish_rotation)
+
+func _finish_rotation() -> void:
+	_is_rotating_camera = false
+	# Normalize the angle to prevent it from growing infinitely
+	_current_angle = fposmod(_current_angle, TAU)
+	_target_angle = _current_angle
